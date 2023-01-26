f02644bb42790b6df5be0b00cea10a792d6ffa93
Schmidt <lschmidt@una.edu>
2023-01-26 16:41:17 -0600

    thursday 1/26

--- app.js
+++ app.js
@@ -8,20 +8,26 @@ let myName = 'Lane';
 
 app.get('/', function (req, res) {
     // res.send('<h1>Hello World, this is Lane in November!')
-    res.send(`<h3> Hi, ${ myName } </h3>`)
+    res.send(`<h3> Hi, ${myName} </h3>`)
     // res.send(myName)
 })
 
 app.get('/show', (req, res) => {
-    
-    res.sendFile('index.html', {root : __dirname});
+
+    res.sendFile('index.html', { root: __dirname });
 })
 
 app.get('/ejs', (req, res) => {
-    
-    // ejs stuff here
-    res.render('index');
 
-}) 
+    // ejs stuff here
+    res.render('index', { myName: myName }); // left var is ejs, right is node land
+
+})
+
+app.get('/name', (req, res) => {
+
+    console.log("in get to slash name:", req.query.ejsFormName);
+
+})
 
 app.listen(PORT)
